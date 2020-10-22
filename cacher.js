cdcc8c056410d96b3c69860bbae8edda72062daa
Shyaboi <ian.shane.sears@gmail.com>
2020-10-21 22:39:15 -0700

    set home / route

--- cacher.js
+++ cacher.js
@@ -14,7 +14,7 @@ app.use(function (req, res, next) {
     next();
   });
 
-  app.get("/multi/:number?", (req, res) => {
+  app.get("/", (req, res) => {
  
     res.send('This is a headless server of an app ( ͡° ͜ʖ ͡°)');
   });
